cce4ad8e71e625041f923d89e951d52676605a7c
Theresa Stocking <nottodayjose@MacBook-Pro.local>
2019-03-18 15:02:53 -0500

    Step 3 Kinda

--- Part1/index.js
+++ Part1/index.js
@@ -1,15 +1,23 @@
-console.log("Hello World");
+console.log("Hello Me");
 
-const play(val) => {
+let progress = [];
+
+function play(val) {
     const getPlayer = document.getElementById("player");
     const element = document.getElementById(val);
+    console.log("val: = " + val);
+    
         if (getPlayer.innerText === "X"){
+            progress[val] = getPlayer.innerText;
             getPlayer.innerText = "O"
             element.innerText = "X";
         } else {
+        progress[val] = getPlayer.innerText;
         getPlayer.innerText = "X"
         element.innerText = "O"
+        
         }
+        console.log(progress);
 }
 
 

55358804d04643c45c2bac43a0eb5cdb7e4e9efb
Theresa Stocking <nottodayjose@MacBook-Pro.local>
2019-03-18 15:29:59 -0500

    Winner If Statement Try 1

--- Part1/index.js
+++ Part1/index.js
@@ -18,11 +18,34 @@ function play(val) {
         progress[val] = getPlayer.innerText;
         getPlayer.innerText = "X"
         element.innerText = "O"
-        
         }
-    }
+    }   
         console.log(progress);
-}
+    if (progress[0] === progress[1] && progress[1] === progress[2]) {
+        alert("We have a winner - " + progress[0])
+    } else if (progress[3] === progress[4] && progress[4] === progress[5]) {
+        alert("We have a winner - " + progress[3]) 
+    } else if (progress[6] === progress[7] && progress[7] === progress[8]) {
+        alert("We have a winner - " + progress[6]) 
+    } else if (progress[0] === progress[3] && progress[3] === progress[6]) {
+        alert("We have a winner - " + progress[0]) 
+    } else if (progress[1] === progress[4] && progress[4] === progress[7]) {
+        alert("We have a winner - " + progress[1])
+    } else if (progress[2] === progress[5] && progress[5] === progress[8]) {
+        alert("We have a winner - " + progress[2])
+    } else if (progress[0] === progress[4] && progress[4] === progress[8]) {
+        alert("We have a winner - " + progress[0])
+    } else if (progress[2] === progress[4] && progress[4] === progress[6]) {
+        alert("We have a winner - " + progress[2]) 
+    } else if (progress.every(notNull)) {
+        alert("We have a draw. . . ") 
+    } 
+
+    function notNull(element, index, array) {
+        return(element === "X" || element ==="O")
+    }
+
+
 
 
 

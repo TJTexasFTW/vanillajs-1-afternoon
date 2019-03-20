028105cb08e6e1525b5cf03a6b723aa268b79af0
TJ <nottodayjose@yahoo.com>
2019-03-19 20:48:48 -0500

    Fixed
    Team effort

--- Part2/index.js
+++ Part2/index.js
@@ -1,16 +1,18 @@
 // console.log("Please let this go fast");
 // console.log("The House ALWAYS wins!")
 
-const cardID_JS = document.getElementById("cardID").value;
+const cardID_JS = document.getElementById("cardID");
 
 const styleAssign_JS = document.getElementById("styleAssign");
-console.log("STYLE: " + styleAssign_JS);
+// console.log("STYLE: " + styleAssign_JS);
 
 function setCard() {
     const card = document.getElementById(cardID_JS.value);
+    console.log("Check1: " + document.getElementById(cardID_JS.value))
     console.log("CARD: " + card);
-    // card.style.color = styleAssign_JS.value;
-    document.getElementById("diamonds").innerHTML = cardID_JS;
+    card.style.color = styleAssign_JS.value;
+    // document.getElementById("diamonds").innerHTML = cardID_JS;
+    // document.getElementById.innerHTML = cardID_JS;
 }
 
 console.log("CARD ID: " + cardID_JS);
